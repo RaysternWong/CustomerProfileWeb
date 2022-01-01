a3e8f5118d038c472af0d935b7c40f256ef54266
Raystern <raysternwong@gmail.com>
2022-01-01 23:18:56 +0800

    Added db context

---
+++ CustomerProfileWeb/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomerProfileWeb.Models
+{
+    public class Customer
+    {
+        [Key]
+        public Int64 CustomerId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Address { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+
+        public DateTime YearOfBorn { get; set; }
+
+        public int PhoneNo { get; set; }
+
+    }
+}
---
+++ CustomerProfileWeb/Models/CustomerContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CustomerProfileWeb.Models
+{
+    public class CustomerContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set;}
+
+        public CustomerContext(DbContextOptions<CustomerContext> option) : base(option)
+        {
+
+        }
+    }
+}
